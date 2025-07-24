7c815c87974e9ae866a57f0fe633e2f353b78d68
JoelCDL <jhagedorn@ucop.edu>
2025-07-24 12:39:28 -0700

    Integrate Orcid ID component into bio component

--- src/generated-content/biogrid.js
+++ src/generated-content/biogrid.js
@@ -8,6 +8,8 @@ for (let i = 0; i < 6; i++) {
   const lastName = faker.person.lastName()
   const name = firstName + ' ' + lastName
   const email = firstName + '.' + lastName + '@ucop.edu'
+  const numberGroup1 = faker.number.int({ min: 1000, max: 9000 })
+  const numberGroup2 = faker.number.int({ min: 1000, max: 9000 })
 
   list.push({
     bio: {
@@ -15,6 +17,9 @@ for (let i = 0; i < 6; i++) {
       email: email,
       image: faker.image.urlPicsumPhotos({ width: 600, height: 400, grayscale: false, blur: 0 }),
       paragraphs: faker.person.jobTitle() + ', ' + faker.person.bio() + '. ' + faker.lorem.sentences(2)
+    },
+    orcid: {
+      record: 'http://orcid.org/0000-0002' + '-' + numberGroup1 + '-' + numberGroup2
     }
   })
 }
