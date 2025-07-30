09d4b785ac37b44076ab03c857e5094d28bb1764
JoelCDL <jhagedorn@ucop.edu>
2025-07-30 11:55:16 -0700

    Create link element styling

---
+++ src/generated-content/links.js
@@ -0,0 +1,9 @@
+// Link Elements //
+
+const { faker } = require('@faker-js/faker')
+
+module.exports = {
+  text_before: faker.lorem.words(),
+  text: faker.lorem.words(),
+  text_after: faker.lorem.words()
+}
