58035238dc26d19f8ae50c5335bdde79988dba7d
JoelCDL <jhagedorn@ucop.edu>
2025-07-30 09:52:03 -0700

    Add breadcrumb nav to secondary template

--- src/generated-content/secondary.js
+++ src/generated-content/secondary.js
@@ -6,5 +6,14 @@ const heading = faker.lorem.sentence(6).replace('.', '')
 module.exports = {
   parallax_image: faker.image.urlPicsumPhotos({ width: 800, height: 400, grayscale: false, blur: 0 }),
   heading: heading,
-  paragraphs: faker.lorem.paragraphs(20, '</p>\n<p>')
+  paragraphs: faker.lorem.paragraphs(20, '</p>\n<p>'),
+  secondary_breadcrumb: {
+    breadcrumb: {
+      text: [
+        'UC3',
+        'Secondary Page',
+        'Another Page'
+      ]
+    }
+  }
 }
