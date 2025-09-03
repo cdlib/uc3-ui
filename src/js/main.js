fde4eec1953d092c19b979e397436d543f125375
JoelCDL <jhagedorn@ucop.edu>
2025-09-03 11:05:19 -0700

    Move anchor js to separate file imported by main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,3 @@
-// ***** Anchor Positioning Polyfill ***** //
+// ***** Main Import of JavaScript Files ***** //
 
-// Required for positioning elements that use the popover API. See https://github.com/oddbird/css-anchor-positioning?tab=readme-ov-file#configuration
-
-const anchorPositioningPolyfill = async () => {
-  const { default: polyfill } = await import('@oddbird/css-anchor-positioning/dist/css-anchor-positioning-fn.js')
-
-  polyfill({
-    elements: undefined,
-    excludeInlineStyles: false,
-    useAnimationFrame: false
-  })
-}
-
-if (!('anchorName' in document.documentElement.style)) {
-  anchorPositioningPolyfill()
-}
+import './*.js'
