ae4eff5b1510546800350139882825a990f4608a
JoelCDL <jhagedorn@ucop.edu>
2025-09-05 15:30:06 -0700

    Set popover fallback css only if api isn’t detected
    Fixes #3

--- src/js/popover-detection.js
+++ src/js/popover-detection.js
@@ -1,10 +1,10 @@
 // ***** Popover Detection ***** //
 
 /*
-If the popover property is detected in the browser, remove the no-popover class from the html tag. This will
+If the popover property is not detected in the browser, add the no-popover class to the html tag. This will
 disable CSS styles using the no-popover class that set alternative UI states if the popover API isn't supported.
 */
 
-if (window.HTMLElement.prototype.hasOwnProperty('popover')) { // eslint-disable-line no-prototype-builtins
-  document.querySelector('html').classList.remove('no-popover')
+if (!window.HTMLElement.prototype.hasOwnProperty('popover')) { // eslint-disable-line no-prototype-builtins
+  document.querySelector('html').classList.add('no-popover')
 }
